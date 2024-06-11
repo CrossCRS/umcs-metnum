7a9e657513d5c1f3b31a58d9cb019b9b2188e126
CrossCRS <cross@linux.pl>
2024-06-11 15:39:22 +0200

    add stuff

---
+++ stuff.cpp
@@ -0,0 +1,15 @@
+// Dokładność - dopuszczalny błąd wyniku, najczęściej ustalany arbitralnie na podstawie doświadczenia.
+// Zbieżność iteracji - Stopniowe zbliżanie się do wyniku w miarę wykonywania kolejnych iteracji. Poszukiwanie kończy się, gdy osiągnięta zostanie zadowalająca dokładność.
+// Dyskretyzacja - zastąpienie obiektu ciągłego obiektem nieciągłym.
+// Model - uproszczona reprezentacja rzeczywistości.
+// Model matematyczny - reprezentacja fragmentu rzeczywistości za pomocą obiektów matematycznych.
+
+// Błąd - różnica między wartością rzeczywistą a wartością obliczoną.
+// Błąd bezwzględny - wartość bezwzględna ^ (|wartość rzeczywista - wartość obliczona|).
+// Błąd względny - stosunek błędu bezwzględnego do wartości rzeczywistej. (błąd bezwzględny / wartość rzeczywista).
+// Przykłady źródeł błędów:
+//      - błędy zaokrągleń
+//      - błędy obcięcia (np. konieczność zakończenia nieskończonych obliczeń)
+//      - błędy wynikające z niedokładności danych wejściowych
+//      - błędy wynikające z niedokładności modelu matematycznego
+
